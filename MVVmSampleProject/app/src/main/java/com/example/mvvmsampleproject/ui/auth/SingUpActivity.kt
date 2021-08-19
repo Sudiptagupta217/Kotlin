6bdadc9c7b23e2513d096a30a8d5a82888e5b6fd
Sudiptagupta217 <Sudiptagupta217@gmail.com>
2021-08-19 18:55:24 +0530

    mvvm demo project

---
+++ MVVmSampleProject/app/src/main/java/com/example/mvvmsampleproject/ui/auth/SingUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mvvmsampleproject.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvvmsampleproject.R
+
+class SingUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up)
+    }
+}
